c3f9a6a33c7d35978c3a372cbb0734a5916bacc4
Hisham Ali <hisham@otofacts.com>
2024-02-13 04:58:40 -0600

    tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
