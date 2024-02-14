85d37d7c151249c25ffeda40874cf6a584ddfafe
Hisham Ali <hisham@otofacts.com>
2024-02-14 08:10:58 -0600

    queue auth

--- src/lib/middlewares/auth-api.ts
+++ src/lib/middlewares/auth-api.ts
@@ -1,4 +1,5 @@
 import { env } from '@environment';
+import { receiver } from '@lib/clients/queue';
 import { isApiRoute } from '@lib/constants/urls';
 import type { MiddlewareHandler } from 'astro';
 
@@ -17,8 +18,18 @@ export const middleware: MiddlewareHandler = async ({ request }, next) => {
   // Check if the request has the correct auth header
   const url = new URL(request.url);
   const authHeader = request.headers.get('authorization');
-  const bearerToken = url.pathname.startsWith('/api/cron/') ? env.CRON_SECRET : env.API_SECRET;
-  if (authHeader !== `Bearer ${bearerToken}`) {
+
+  let valid = true;
+  if (url.pathname.startsWith('/api/crons/')) {
+    valid = authHeader !== `Bearer ${env.CRON_SECRET}`;
+  } else if (url.pathname.startsWith('/api/queues/')) {
+    const signature = request.headers.get('upstash-signature');
+    valid = !!signature && (await receiver.verify({ signature, body: await request.text() }));
+  } else {
+    valid = authHeader !== `Bearer ${env.API_SECRET}`;
+  }
+
+  if (!valid) {
     return new Response('Unauthorized', { status: 401 });
   }
 
